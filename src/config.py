316c7b02b5de7db35309fdbda7c68b0fc19d3651
EctoPLSM <wpc.unofficial@gmail.com>
2021-04-18 12:14:55 +0900

    Reflecting yet another WPC postponement
    :((((((((((

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ webmaster_email = "wpc.unofficial@gmail.com"
 # Technically, these can be non-consecutive
 # Thanks COVID, now I can exploit the feature
 previous_year = "2019"
-next_year = "2021"
+next_year = "2022"
 
 # On github, the links don't have to have .html prefix,
 #   and directories forward to index.html inside.
