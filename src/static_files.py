a7b4a3bfdbe40a30a0adc19013feffb78a66cd5b
EctoPLSM <57141046+EctoPLSM@users.noreply.github.com>
2023-06-13 07:03:35 +0900

    Fixed the address to WPF website, added 2023 date

--- src/static_files.py
+++ src/static_files.py
@@ -4,9 +4,9 @@ import subprocess
 def run():
     print("Copying static files")
     # shutils pls no :(
-    subprocess.Popen("cp -r ./templates/img ../", shell=True)
-    subprocess.Popen("cp -r ./templates/css ../", shell=True)
-    subprocess.Popen("cp -r ./templates/pdfs ../", shell=True)
+    subprocess.Popen("copy -r ./templates/img ../", shell=True)
+    subprocess.Popen("copy -r ./templates/css ../", shell=True)
+    subprocess.Popen("copy -r ./templates/pdfs ../", shell=True)
 
 
 if __name__ == "__main__":
